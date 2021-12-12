c2e879774a606030a8e97ce10afbb5a6ae48b56d
kishore <kishore07kzr@gmail.com>
2021-12-12 16:20:18 +0530

    ver1.0

--- app.js
+++ app.js
@@ -5,27 +5,27 @@ const reviews = [
     name: "Dannush",
     job: "Doli Simp",
     img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+      "dan.jpg",
     text:
-      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+      "Kallarayil⚰️Kooda Jannal Ondru Veithu.. Undhan Mugam👰🏻 Parpen Adi!. Wommaleee Yaara da Otha-ndra🤼 Kai Adi, Rasam Kudi, Dolis Varum Unai Thedi😎",
   },
-  {
+  { 
     id: 2,
     name: "KathirNilavan",
     job: "Pick-Up Artist",
     img:
       "/kthab.jpg",
     text:
-      "Habibi Come to Dubaii!!. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+      "Habibi Come to Dubaii!🔥. Lets go Party With Bitches👯‍♀️.AdangaMaru AthuMeeru Doligalai👅 potu vaalvu kudu. DM for Porn Videos 👉👌",
   },
   {
     id: 3,
     name: "Kishore",
     job: "Boomer",
     img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+      "/kishore.jpg",
     text:
-      "Boomer Ungle!, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+      "Boomer Ungle🤡!, Na Our Alien! and I'm Just Having Fun👽 PaanParak Saapdunga Frens🧡 Divyaa....Divyaaa....😚",
   },
   {
     id: 4,
@@ -34,7 +34,7 @@ const reviews = [
     img:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
     text:
-      "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+      "Blow Job Expert👄, Dannush Supremacy🛐, DM for Low Budget Ool💦, Pool-uh Perusa irundha Discount Bha🍆 ",
   },
 ];
 
